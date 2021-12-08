bc33ab10a7e67f1c790ce0b11462e197d897e0da
ikolodiazhnyi <ikolodiazhnyi@gmail.com>
2021-12-08 21:09:23 +0200

    finish styling navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,11 +1,32 @@
 import React from "react";
-import { Nav, NavbarContainer, NavLogo } from "./NavbarElement";
+import {FaBars} from "react-icons/fa"
+import { MobileIcon, Nav, NavbarContainer, NavBtn, NavBtnLink, NavItem, NavLinks, NavLogo, NavMenu } from "./NavbarElement";
 
 const Navbar = () => {
   return (
     <Nav>
       <NavbarContainer>
         <NavLogo to="/">logo</NavLogo>
+        <MobileIcon>
+          <FaBars />
+        </MobileIcon>
+        <NavMenu>
+          <NavItem>
+            <NavLinks to="about">About</NavLinks>
+          </NavItem>
+          <NavItem>
+            <NavLinks to="discover">Discover</NavLinks>
+          </NavItem>
+          <NavItem>
+            <NavLinks to="services">Services</NavLinks>
+          </NavItem>
+          <NavItem>
+            <NavLinks to="signup">Sign Up</NavLinks>
+          </NavItem>
+        </NavMenu>
+        <NavBtn>
+          <NavBtnLink to="/signin">Sign In </NavBtnLink>
+        </NavBtn>
       </NavbarContainer>
     </Nav>
   );
