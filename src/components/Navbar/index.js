519b17d687ae0983b0d4f3a41265a97d32533fef
ikolodiazhnyi <ikolodiazhnyi@gmail.com>
2021-12-08 18:36:38 +0200

    make nav bar and add logo

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React from "react";
+import { Nav, NavbarContainer, NavLogo } from "./NavbarElement";
 
 const Navbar = () => {
   return (
-    <div>
-      <h1>Navbar</h1>
-    </div>
-  )
-}
+    <Nav>
+      <NavbarContainer>
+        <NavLogo to="/">logo</NavLogo>
+      </NavbarContainer>
+    </Nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
